a90ce740d0e119a01403363c8ccfd3d931bbafc1
edu02 <edu02@EDUPC02.n00d01.kis.co.jp>
2020-04-22 13:19:38 +0900

    first

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int month=4;
+		int day=22;
+		System.out.println("今日ハ" + month + "月" + day + "日デス");
+
+		double p=3.14;
+		System.out.println("円周率ハ" + p + "デス");
+	}
+
+}
