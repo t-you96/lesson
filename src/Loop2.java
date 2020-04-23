d6d833418731bd0ac79c99d99b6ac85d952832b6
edu02 <edu02@EDUPC02.n00d01.kis.co.jp>
2020-04-23 11:30:07 +0900

    first

---
+++ src/Loop2.java
@@ -0,0 +1,33 @@
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//1.こんにちは と五回表示
+//		for(int a=0 ; a<=5 ; a++){
+//			if (a==5) {
+//				break;
+//			}
+//			System.out.println("こんにちは");
+//		}
+//	
+		
+		//2.1 2 3 4 5 と表示
+//		for(int b=1 ; b<=5 ; b++){
+//			System.out.print(b + " ");
+//		}	
+//		
+		//3.numに数を入れて0からnumまで表示
+		int n=new java.util.Scanner(System.in).nextInt();
+		for(int c=0 ; c<=n ; c++){
+			System.out.println(c);
+		}
+	
+		
+		//4.10以下の偶数を表示(2 4 6 8 10)
+		//5.5 4 3 2 1
+	
+	}
+
+}
