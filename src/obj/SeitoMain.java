3182608e02b8d52c454775870473b637571d0ee0
edu02 <edu02@EDUPC02.n00d01.kis.co.jp>
2020-04-27 17:06:07 +0900

    生徒点数3.0小数点第二位以下を四捨五入

--- src/obj/SeitoMain.java
+++ src/obj/SeitoMain.java
@@ -3,20 +3,20 @@ package obj;
 public class SeitoMain {
 
 	public static void main(String[] args) {
-		
+
 		Seito st = new Seito();
-		
+
 		st.name = "北川";
-		st.kokugo = 92;
+		st.kokugo = 95;
 		st.math = 61;
 		st.society = 70;
 		st.show();
-		
+
 		System.out.println();
-		
+
 		System.out.println("合計は" + st.goukei() + "点");
-		
-		System.out.println("平均は" + st.heikin() + "点");
+
+		System.out.println("平均は" + String.format("%.1f", st.heikin()) + "点");
 
 	}
 
