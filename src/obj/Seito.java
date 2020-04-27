79b183ddcb8b0d4cb1c4872a9940b25053378e9a
edu02 <edu02@EDUPC02.n00d01.kis.co.jp>
2020-04-27 16:46:43 +0900

    生徒点数1.0

---
+++ src/obj/Seito.java
@@ -0,0 +1,24 @@
+package obj;
+
+public class Seito {
+
+	String name;
+	int kokugo;
+	int math;
+	int society;
+
+	public void show() {
+		System.out.print(name + " 国語" + kokugo + "点 数学" + math + "点 社会" + society + "点" );
+	}
+
+	public int goukei() {
+		int g = kokugo + math + society;
+		return g;
+	}
+
+	public double heikin() {
+		double h = (kokugo + math + society) / 3;
+		return h;
+	}
+
+}
---
+++ src/obj/SeitoMain.java
@@ -0,0 +1,22 @@
+package obj;
+
+public class SeitoMain {
+
+	public static void main(String[] args) {
+		
+		Seito st = new Seito();
+		
+		st.name = "北川";
+		st.kokugo = 90;
+		st.math = 60;
+		st.society = 70;
+		st.show();
+		
+		System.out.println();
+		
+		System.out.println("合計は" + st.goukei() + "点");
+		System.out.println("平均は" + st.heikin() + "点");
+
+	}
+
+}
